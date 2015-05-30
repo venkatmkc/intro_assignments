fa7a7efda5315dcb7f129354d8a794011216c7a6
Venkatmkc <venkatmkc@gmail.com>
2015-05-30 15:55:25 +0530

    after the prints

--- Main.java
+++ Main.java
@@ -3,17 +3,20 @@ import java.util.ArrayList;
 public class Main {
     public static void simplest_exercise(){
         System.out.println("*");
+        System.out.println();
     }
     public static void horizontal_line(int length)
     {
         for(int i=0;i<length;i++)
             System.out.print("*");
         System.out.println();
+        System.out.println();
     }
     public static void vertical_line(int length)
     {
         for(int i=0;i<length;i++)
             System.out.println("*");
+        System.out.println();
     }
     public static  void right_triangle(int length)
     {
@@ -23,6 +26,7 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println();
     }
     public static void isosceles_triange(int length)
     {
@@ -34,6 +38,7 @@ public class Main {
                 System.out.print("*");
             System.out.println();
         }
+        System.out.println();
     }
     public static void diamond(int length) {
         for (int i = 0; i < length; i++) {
@@ -50,6 +55,7 @@ public class Main {
                 System.out.print("*");
             System.out.println();
         }
+        System.out.println();
     }
     public static void diamond_with_name(int length,String name)
     {
@@ -70,6 +76,7 @@ public class Main {
                 System.out.print("*");
             System.out.println();
         }
+        System.out.println();
     }
     public static void fizzbuzz()
     {
@@ -84,6 +91,7 @@ public class Main {
             else
                 System.out.println(i);
         }
+        System.out.println();
     }
     public static ArrayList<Integer> prime_factors(int number)
     {
@@ -113,24 +121,17 @@ public class Main {
         {
             System.out.print(i+" ");
         }
+        System.out.println();
     }
     public static void main(String[] args) {
         simplest_exercise();
-        System.out.println();
         horizontal_line(5);
-        System.out.println();
         vertical_line(3);
-        System.out.println();
         right_triangle(3);
-        System.out.println();
         isosceles_triange(3);
-        System.out.println();
         diamond(3);
-        System.out.println();
         diamond_with_name(3, "Venkat");
-        System.out.println();
         fizzbuzz();
-        System.out.println();
         print_list(prime_factors(3));
     }
 }
